6d9cc19db199739905d6c38c858013001852684a
zhang5773117 <40231462+zhang5773117@users.noreply.github.com>
2019-03-06 17:18:33 +0800

    11
    11

--- myperson-manager/myperson-manager-common/src/main/java/com/zb/config/RedisConfig.java
+++ myperson-manager/myperson-manager-common/src/main/java/com/zb/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+    //张三
     //一个工具类
     @Bean
     public RedisTemplate<String, Object> create(RedisConnectionFactory f) {
