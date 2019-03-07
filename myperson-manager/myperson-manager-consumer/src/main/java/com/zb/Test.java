086ecfe41c5e75f7792af29ea4312761c45b4ff7
zhang5773117 <40231462+zhang5773117@users.noreply.github.com>
2019-03-07 11:05:16 +0800

    aa
    啊啊

--- myperson-manager/myperson-manager-consumer/src/main/java/com/zb/Test.java
+++ myperson-manager/myperson-manager-consumer/src/main/java/com/zb/Test.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //测试123
 @SpringBootApplication
 public class Test {
-
+    //hub
     public static void main(String[] args) {
         SpringApplication.run(Test.class, args);
     }
