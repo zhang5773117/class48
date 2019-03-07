9f26c927874c752caf545a6eae4ba1f23d074cf4
zhang5773117 <40231462+zhang5773117@users.noreply.github.com>
2019-03-07 11:04:26 +0800

    123
    123

--- myperson-manager/myperson-manager-consumer/src/main/java/com/zb/Test.java
+++ myperson-manager/myperson-manager-consumer/src/main/java/com/zb/Test.java
@@ -3,6 +3,7 @@ package com.zb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//张三
 //测试123
 @SpringBootApplication
 public class Test {
